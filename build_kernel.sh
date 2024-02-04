addd4f9f4a49a1fbab93bd9b5d40bb29914469dc
renoxtv <guyguygftyfty@gmail.com>
2024-02-04 18:35:30 +0200

    update build scripts and gitignore #2

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,15 +4,10 @@ export ARCH=arm64
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
-
-TOOLCHAIN_PATH=~/kernel/toolchain
-
-PATH="$TOOLCHAIN_PATH/clang/bin:$TOOLCHAIN_PATH/aarch64-linux-android-4.9/bin:$TOOLCHAIN_PATH/arm-linux-androideabi-4.9/bin:${PATH}"
-
-make ARCH=arm64 exynos2100-p3sxxx_defconfig
-make -j$(nproc --all) \
-        ARCH="arm64" \
-        CC="clang" \
-        CLANG_TRIPLE="aarch64-linux-gnu-" \
-        CROSS_COMPILE="aarch64-linux-android-" \
-        CROSS_COMPILE_ARM32="arm-linux-androideabi-"
+SRC_DIR=$(pwd)
+TC_DIR=/home/renox/kernel/toolchain/clang
+JOBS=$(nproc)
+MAKE_PARAMS="-j$JOBS -C $SRC_DIR O=$SRC_DIR/ ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 CROSS_COMPILE=$TC_DIR/bin/llvm-"
+export PATH="$TC_DIR/bin:$PATH"
+make $MAKE_PARAMS exynos2100-p3sxxx_defconfig
+make $MAKE_PARAMS
